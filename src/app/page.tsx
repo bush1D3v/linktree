a3598a3244bacec9e457748a6efc8f868b83c44d
Victor José Lopes Navarro <victor.jose.lopes.navarro@gmail.com>
2025-11-24 13:59:22 -0300

    Update TikTok link in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
         </li>
         <li>
           <LinkButton
-            href="https://www.tiktok.com/devnavarro"
+            href="https://www.tiktok.com/@devnavarro"
             text="TikTok"
             icon={<FaTiktok />}
           />
